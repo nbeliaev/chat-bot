8cee06a000fdb5507cc3b2eb58dedf00aaef383d
Nick <fr13.dev@gmail.com>
2019-11-05 14:51:19 +0300

    feature, add background job on start

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,16 +1,19 @@
 package main;
 
+import backgroundjob.BackgroundJob;
+import backgroundjob.BackgroundJobRunner;
 import configs.Config;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.SimpleServlet;
+import servlets.DialogflowServlet;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        new BackgroundJobRunner(BackgroundJob.class).runJob();
         final ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(new SimpleServlet()), "/");
+        context.addServlet(new ServletHolder(new DialogflowServlet()), "/");
         final Server server = new Server(Integer.parseInt(Config.getProperty(Config.PORT)));
         server.setHandler(context);
         server.start();

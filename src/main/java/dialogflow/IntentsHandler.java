7c3eb83ef19f9a7d994d4f728abbe325869f81ce
Nick <fr13.dev@gmail.com>
2019-11-11 15:37:55 +0300

    feature, add handler for active ingredient intent

--- src/main/java/dialogflow/IntentsHandler.java
+++ src/main/java/dialogflow/IntentsHandler.java
@@ -7,7 +7,9 @@ import com.google.actions.api.ForIntent;
 import com.google.actions.api.response.ResponseBuilder;
 import com.google.api.services.actions_fulfillment.v2.model.SimpleResponse;
 import database.dao.Dao;
+import database.dao.ProductDaoImpl;
 import database.dao.StoreDaoImpl;
+import database.entities.ProductEntity;
 import database.entities.StoreEntity;
 
 import java.util.List;
@@ -16,18 +18,35 @@ import java.util.List;
 public class IntentsHandler extends DialogflowApp {
 
     @ForIntent("Shops-all")
-    public ActionResponse welcome(ActionRequest request) {
+    public ActionResponse getAllShops(ActionRequest request) {
         final ResponseBuilder responseBuilder = getResponseBuilder(request);
         Dao<StoreEntity> dao = new StoreDaoImpl();
         final List<StoreEntity> stores = dao.getAll(StoreEntity.class);
         final StringBuilder stringBuilder = new StringBuilder();
-        stores.forEach(storeEntity -> {
-            stringBuilder.append(storeEntity.getName())
+        stores.forEach(entity -> {
+            stringBuilder.append(entity.getName())
                     .append("\n")
                     .append("- Часы работы: Пн-Пт, 8-18")
                     .append("\n")
                     .append("- Т: +79115554875")
                     .append("\n")
+                    .append("\n");
+        });
+        final SimpleResponse simpleResponse = new SimpleResponse();
+        simpleResponse.setDisplayText(stringBuilder.toString());
+        responseBuilder.add(simpleResponse);
+        return responseBuilder.build();
+    }
+
+    @ForIntent("Active Ingredient")
+    public ActionResponse getProductListByActiveIngredient(ActionRequest request) {
+        final String activeIngredient = (String) request.getParameter("activeIngredient");
+        final ResponseBuilder responseBuilder = getResponseBuilder(request);
+        final Dao<ProductEntity> dao = new ProductDaoImpl();
+        final List<ProductEntity> analogs = dao.findByPattern(ProductEntity.class, "activeIngredient", activeIngredient);
+        final StringBuilder stringBuilder = new StringBuilder();
+        analogs.forEach(entity -> {
+            stringBuilder.append(entity.getName())
                     .append("\n");
         });
         final SimpleResponse simpleResponse = new SimpleResponse();

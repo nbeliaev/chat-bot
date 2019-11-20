ba2d318dd37b9e3a9b7b457bb64b6929c6018d8d
Nick <fr13.dev@gmail.com>
2019-11-20 11:29:33 +0300

    refactoring, add catch for uniqueViolation

--- src/main/java/exceptions/ExceptionUtil.java
+++ src/main/java/exceptions/ExceptionUtil.java
@@ -7,7 +7,8 @@ import javax.persistence.PersistenceException;
 public class ExceptionUtil {
     public static DataBaseException convertException(PersistenceException e) {
         // TODO
-        if (e.getCause() instanceof ConstraintViolationException) {
+        final String uniqueViolation = "23505";
+        if (((ConstraintViolationException) e).getSQLState().equals(uniqueViolation)) {
             return new ExistDataBaseException(e.getMessage());
         } else {
             return new DataBaseException(e.getMessage());

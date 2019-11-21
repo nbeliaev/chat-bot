f1bf0f0e0cc66a2a30c1e5c10291c379b8f48650
Nick <fr13.dev@gmail.com>
2019-11-21 16:00:42 +0300

    refactoring, add prices processing

--- src/main/java/backgroundjob/BackgroundJob.java
+++ src/main/java/backgroundjob/BackgroundJob.java
@@ -1,12 +1,15 @@
 package backgroundjob;
 
 import database.dao.Dao;
+import database.dao.PriceDao;
 import database.dao.ProductDao;
 import database.dao.StoreDao;
+import database.entities.PriceEntity;
 import database.entities.ProductEntity;
 import database.entities.StoreEntity;
 import database.externaldata.DataReceiver;
 import exceptions.ConnectionException;
+import exceptions.ExistDataBaseException;
 import exceptions.NotExistDataBaseException;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -17,6 +20,9 @@ import java.util.Arrays;
 
 public class BackgroundJob implements Job {
     private final DataReceiver dataReceiver;
+    private static final String STORES_RESOURCE = "stores";
+    private static final String PRODUCT_RESOURCE = "products";
+    private static final String PRICES_RESOURCE = "prices";
 
     {
         dataReceiver = new DataReceiver();
@@ -30,7 +36,7 @@ public class BackgroundJob implements Job {
     }
 
     private void processStores() throws JobExecutionException {
-        final String json = getJson("stores");
+        final String json = getJson(STORES_RESOURCE);
         final StoreEntity[] stores = JsonParser.read(json, StoreEntity[].class);
         final Dao<StoreEntity> dao = new StoreDao();
         Arrays.stream(stores).forEach(
@@ -46,7 +52,7 @@ public class BackgroundJob implements Job {
     }
 
     private void processProducts() throws JobExecutionException {
-        final String json = getJson("products");
+        final String json = getJson(PRODUCT_RESOURCE);
         final ProductEntity[] products = JsonParser.read(json, ProductEntity[].class);
         final Dao<ProductEntity> dao = new ProductDao();
         Arrays.stream(products).forEach(
@@ -62,18 +68,18 @@ public class BackgroundJob implements Job {
     }
 
     private void processPrices() throws JobExecutionException {
-        final String json = getJson("prices");
-        final ProductEntity[] products = JsonParser.read(json, ProductEntity[].class);
-        final Dao<ProductEntity> dao = new ProductDao();
+        final String json = getJson(PRICES_RESOURCE);
+        final PriceEntity[] products = JsonParser.read(json, PriceEntity[].class);
+        final Dao<PriceEntity> dao = new PriceDao();
+        dao.deleteAll();
         Arrays.stream(products).forEach(
                 // TODO: need to optimise
                 entity -> {
                     try {
-                        //final ProductEntity persistedEntity = dao.findByPattern(ProductEntity.class, "external_id", entity.getExternalId());
-                        //persistedEntity.setStores(entity.getStores());
-                        // dao.update(persistedEntity);
-                    } catch (NotExistDataBaseException e) {
-                        throw new NotExistDataBaseException("not exist");
+                        dao.save(entity);
+                    } catch (ExistDataBaseException e) {
+                        throw new ExistDataBaseException(
+                                String.format("Couldn't save price with uuid %s. Because it already exists.", entity.getUuid()));
                     }
                 });
     }
